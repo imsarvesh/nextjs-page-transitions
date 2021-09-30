21c25ccdab38092ab6d3ba399543792b266f45df
Sarvesh Kesharwani <sarvesh.kesharwani@thehutgroup.com>
2021-09-30 14:43:46 +0100

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import fetch from "isomorphic-unfetch";
 import { motion } from "framer-motion";
+import React from "react";
 
 // Our custom easing
 let easing = [0.6, -0.05, 0.01, 0.99];
@@ -14,75 +15,90 @@ const fadeInUp = {
   initial: {
     y: 60,
     opacity: 0,
-    transition: { duration: 0.6, ease: easing }
+    transition: { duration: 0.6, ease: easing },
   },
   animate: {
     y: 0,
     opacity: 1,
     transition: {
       duration: 0.6,
-      ease: easing
-    }
-  }
+      ease: easing,
+    },
+  },
 };
 
 const stagger = {
   animate: {
     transition: {
-      staggerChildren: 0.1
-    }
-  }
+      staggerChildren: 0.1,
+    },
+  },
 };
 
-const Index = props => (
-  <motion.div initial='initial' animate='animate' exit={{ opacity: 0 }}>
-    <div className='container center'>
-      <motion.div
-        animate={{ opacity: 1 }}
-        initial={{ opacity: 0 }}
-        className='title'>
-        <h1>Select a protein</h1>
-      </motion.div>
-      <motion.div variants={stagger} className='product-row'>
-        {props.products.map(product => (
-          <Link
-            key={product.id}
-            href='/products/[id]'
-            as={`/products/${product.id}`}>
-            <motion.div
-              variants={fadeInUp}
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-              className='card'>
-              <span className='category'>Protein</span>
-              <motion.img
-                initial={{ x: 60, opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                transition={{ delay: 0.2 }}
-                key={product.image}
-                src={product.image}
-                width={250}
-              />
-              <div className='product-info'>
-                <h4>{product.name}</h4>
-                <span>{product.price}</span>
-              </div>
-            </motion.div>
-          </Link>
-        ))}
-      </motion.div>
-    </div>
-  </motion.div>
-);
+const Index = () => {
+  const [products, setProducts] = React.useState([]);
 
-Index.getInitialProps = async function() {
-  const res = await fetch(
-    "http://localhost:3000/api/products"
+  React.useEffect(() => {
+    (async () => {
+      const products = await fetch(`/products.json`).then((res) => res.json());
+      setProducts(products);
+    })();
+  }, []);
+  
+  
+  return (
+    <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
+      <div className="container center">
+        <motion.div
+          animate={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
+          className="title"
+        >
+          <h1>Select a protein</h1>
+        </motion.div>
+        <motion.div variants={stagger} className="product-row">
+          {products.map((product) => (
+            <Link
+              key={product.id}
+              href="/products/[id]"
+              as={`/products/${product.id}`}
+            >
+              <motion.div
+                variants={fadeInUp}
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+                className="card"
+              >
+                <span className="category">Protein</span>
+                <motion.img
+                  initial={{ x: 60, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ delay: 0.2 }}
+                  key={product.image}
+                  src={product.image}
+                  width={250}
+                />
+                <div className="product-info">
+                  <h4>{product.name}</h4>
+                  <span>{product.price}</span>
+                </div>
+              </motion.div>
+            </Link>
+          ))}
+        </motion.div>
+      </div>
+    </motion.div>
   );
-  const data = await res.json();
-  return {
-    products: data
-  };
 };
+
+// Index.getInitialProps = async function() {
+//   // const res = await fetch(
+//   //   `/products.json`
+//   // );
+//   // const data = await res.json();
+//   // return {
+//   //   products: data
+//   // };
+// };
 
 export default Index;
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,97 +1,102 @@
+import React from "react";
 import fetch from "isomorphic-unfetch";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 let easing = [0.6, -0.05, 0.01, 0.99];
 
 const stagger = {
   animate: {
     transition: {
-      staggerChildren: 0.05
-    }
-  }
+      staggerChildren: 0.05,
+    },
+  },
 };
 
 const fadeInUp = {
   initial: {
     y: 60,
     opacity: 0,
-    transition: { duration: 0.6, ease: easing }
+    transition: { duration: 0.6, ease: easing },
   },
   animate: {
     y: 0,
     opacity: 1,
     transition: {
       duration: 0.6,
-      ease: easing
-    }
-  }
+      ease: easing,
+    },
+  },
 };
 
-const Product = props => (
-  <motion.div initial='initial' animate='animate' exit={{ opacity: 0 }}>
-    <div className='fullscreen'>
-      <div className='product'>
-        <motion.div
-          className='img'
-          animate={{ opacity: 1 }}
-          initial={{ opacity: 0 }}>
-          <motion.img
-            key={props.product.image}
-            src={props.product.image}
-            animate={{ x: 0, opacity: 1 }}
-            initial={{ x: 200, opacity: 0 }}
-            exit={{ opacity: 0 }}
-            transition={{ delay: 0.2 }}
-          />
-        </motion.div>
-        <div className='product-details'>
-          <motion.div variants={stagger} className='inner'>
-            <Link href='/'>
-              <motion.div variants={fadeInUp}>
-                <a className='go-back'>Back to products</a>
-              </motion.div>
-            </Link>
-            <motion.div variants={fadeInUp}>
-              <span className='category'>Protein</span>
-            </motion.div>
-            <motion.h1 variants={fadeInUp}>{props.product.name}</motion.h1>
-            <motion.p variants={fadeInUp}>{props.product.details}</motion.p>
-            <motion.div variants={fadeInUp} className='additonals'>
-              <span>Soy Free</span>
-              <span>Gluten Free</span>
-            </motion.div>
-            <motion.div variants={fadeInUp} className='qty-price'>
-              <div className='qty'>
-                <div className='minus'>-</div>
-                <div className='amount'>1</div>
-                <div className='add'>+</div>
-              </div>
-              <span className='price'>{props.product.price}</span>
-            </motion.div>
-            <motion.div variants={fadeInUp} className='btn-row'>
-              <button className='add-to-cart'> Add to cart</button>
-              <button className='subscribe'> Subscribe</button>
-            </motion.div>
+const Product = (props) => {
+  const [product, setProduct] = React.useState();
+  const [loading, setLoading] = React.useState(true);
+  const { query } = useRouter();
+
+  React.useEffect(() => {
+    (async () => {
+      const products = await fetch(`/products.json`).then((res) => res.json());
+      const product = products.find((product) => product.id == query.id);
+      setProduct(product);
+      setLoading(false);
+    })();
+  }, []);
+
+  return loading ? (
+    "."
+  ) : (
+    <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
+      <div className="fullscreen">
+        <div className="product">
+          <motion.div
+            className="img"
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+          >
+            <motion.img
+              key={product.image}
+              src={product.image}
+              animate={{ x: 0, opacity: 1 }}
+              initial={{ x: 200, opacity: 0 }}
+              exit={{ opacity: 0 }}
+              transition={{ delay: 0.2 }}
+            />
           </motion.div>
+          <div className="product-details">
+            <motion.div variants={stagger} className="inner">
+              <Link href="/">
+                <motion.div variants={fadeInUp}>
+                  <a className="go-back">Back to products</a>
+                </motion.div>
+              </Link>
+              <motion.div variants={fadeInUp}>
+                <span className="category">Protein</span>
+              </motion.div>
+              <motion.h1 variants={fadeInUp}>{product.name}</motion.h1>
+              <motion.p variants={fadeInUp}>{product.details}</motion.p>
+              <motion.div variants={fadeInUp} className="additonals">
+                <span>Soy Free</span>
+                <span>Gluten Free</span>
+              </motion.div>
+              <motion.div variants={fadeInUp} className="qty-price">
+                <div className="qty">
+                  <div className="minus">-</div>
+                  <div className="amount">1</div>
+                  <div className="add">+</div>
+                </div>
+                <span className="price">{product.price}</span>
+              </motion.div>
+              <motion.div variants={fadeInUp} className="btn-row">
+                <button className="add-to-cart"> Add to cart</button>
+                <button className="subscribe"> Subscribe</button>
+              </motion.div>
+            </motion.div>
+          </div>
         </div>
       </div>
-    </div>
-  </motion.div>
-);
-
-Product.getInitialProps = async function(context) {
-  const { id } = context.query;
-  const res = await fetch(
-    `http://localhost:3000/api/products/`
+    </motion.div>
   );
-  
-  const products = await res.json();
-
-  const product = products.find(product => product.id == id)
-
-  console.log({product})
-  return { product };
 };
-
 export default Product;
